5b48b32e24baf2f57bad39eec415fc50f20e1e65
Dipakshi Saxena <75835612+Dipakshi26@users.noreply.github.com>
2022-05-25 12:28:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOn2FileHandling
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj1 = new Class1();
+            obj1.Readfile();
+            Console.WriteLine();
+            Console.WriteLine("read operation completed");
+        }
+    }
+}
